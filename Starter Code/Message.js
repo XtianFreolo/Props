f86d21188636a6eadd00c471a04a5f2b86fe595f
XtianFreolo <f60@umbc.edu>
2025-05-19 19:43:00 -0400

    rendered children at Message.js and styled it

--- Starter Code/Message.js
+++ Starter Code/Message.js
@@ -1,8 +1,8 @@
-function Message (/* TODO: Take the props. */)
+function Message ({ children }) 
 {
 	return (
-		<div>
-			<!-- TODO: Render children. -->
+		<div style={{ color: 'teal', fontWeight: 'bold', marginTop: '5px'}}>
+			{children}
 		</div>
 	);
 }
