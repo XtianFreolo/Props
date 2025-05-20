21a9e96aa660a998837d726565961fb34fcb5b73
XtianFreolo <f60@umbc.edu>
2025-05-19 20:22:27 -0400

    Finished Inventory Items, styled inventory items

--- Starter Code/InventoryItem.js
+++ Starter Code/InventoryItem.js
@@ -1,10 +1,25 @@
-function InventoryItem (/* TODO: Take the props. Set defaults to the quantity. */)
+function InventoryItem ({ name, type, quantity = 0, price = 0 })
 {
+	// To calculate inventory these are set
+	const totalValue = quantity * price;
+	const isLowStock = quantity <= 5; 
+	const isHighValue = totalValue > 1000;
+
 	return (
-		<div>
-			<h2><!-- TODO: Render the item's details. --></h2>
-			<!-- TODO: Render the low stock alert based on the quantity of the item. -->
-			<!-- TODO: Render the high value alert based on the total value of the item. -->
+		<div style ={{ border: '1px solid gray', padding: '10px', marginBottom: '10px' }}>
+			<h2>{name}</h2>
+			<p><strong>Type:</strong> {type} </p>
+			<p><strong>Quantity:</strong> {quantity} </p>
+			<p><strong>Price:</strong> {price} </p>
+
+			{isLowStock && (
+				<Message> Warning!! Low on Stock </Message>
+			)}
+
+			{isHighValue && (
+				<Message> Caution: High Value Item, Protect from theives and Raccoons </Message>
+			)}
+
 		</div>
 	);
 }
